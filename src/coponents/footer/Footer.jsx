a6bca20efd44f626849a4127ba4f157316a1df6c
melashu <meshu102@outlook.com>
2022-10-14 07:25:43 +0300

    Add footer component that display at the bottom page

---
+++ src/coponents/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <footer>
+      <div className="top" data-testid="footer">
+        Designed By <a href="https://github.com/melashu" target={"_blank"}>Meshu Amare</a>{" "}
+      </div>
+      <div className="bottom">Copywrite &copy; 2022;</div>
+    </footer>
+  );
+};
+
+export default Footer;
