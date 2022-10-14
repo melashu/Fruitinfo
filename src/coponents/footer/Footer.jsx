5459f954b1623b31f8a18c29529ed0858d59fd68
melashu <meshu102@outlook.com>
2022-10-14 08:10:34 +0300

    Add fix for stylelint error

--- src/coponents/footer/Footer.jsx
+++ src/coponents/footer/Footer.jsx
@@ -6,7 +6,9 @@ const Footer = () => (
     <div className="top" data-testid="footer">
       Designed By
       {' '}
-      <a href="https://github.com/melashu" target="_blank" rel="noreferrer">Meshu Amare</a>
+      <a href="https://github.com/melashu" target="_blank" rel="noreferrer">
+        Meshu Amare
+      </a>
       {' '}
     </div>
     <div className="bottom">Copywrite &copy; 2022;</div>
