4a8e18034ebb673152f12f7b743ab2364da4b590
Meshu <meshu102@outlook.com>
2022-10-14 12:09:09 +0300

    Update fruitreducer.jsx
    Update the API endpoint path.

--- src/redux/fruitreducer.jsx
+++ src/redux/fruitreducer.jsx
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // const axios = require("axios");
 const fruitThunk = createAsyncThunk("fruitinfo/fruitThunk", async () => {
   try {
-    const response = await fetch("api/fruit/all",{method:"GET"});
+    const response = await fetch("https://www.fruityvice.com/api/fruit/all",{method:"GET"});
     const data = await response.json();
     return data;
   } catch (error) {
