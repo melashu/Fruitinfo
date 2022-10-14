7550dbe9830b0a23771f0ddee08a8faad9df602f
melashu <meshu102@outlook.com>
2022-10-14 07:30:16 +0300

    Create root reducer using redux toolkit

---
+++ src/redux/fruitreducer.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+// const axios = require("axios");
+const fruitThunk = createAsyncThunk("fruitinfo/fruitThunk", async () => {
+  try {
+    const response = await fetch("api/fruit/all",{method:"GET"});
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+});
+const initialState = {
+  fruit: [],
+  loadingStatus: "idel",
+};
+
+const fruitReducer = createSlice({
+  name: "fruitinfo",
+  initialState,
+  reducers: {
+    selectedFruit: (state) => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fruitThunk.fulfilled, (state, { payload }) => {
+        state.fruit = payload;
+        state.loadingStatus = "success";
+      })
+      .addCase(fruitThunk.pending, (state) => {
+        state.loadingStatus = "pending";
+      })
+      .addCase(fruitThunk.rejected, (state) => {
+        state.loadingStatus = "rejected";
+      });
+  },
+});
+
+const getLoading = (state) => state.fruit.loadingStatus;
+const getFruitInfo = (state) => state.fruit.fruit;
+
+export { getLoading, getFruitInfo, fruitThunk };
+export default fruitReducer.reducer;
