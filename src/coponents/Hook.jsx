22a9d39ff4c46c37604db30222cdf71ac507c042
melashu <meshu102@outlook.com>
2022-10-14 07:26:48 +0300

    Creata a function for useSelector and useDispatch hook

---
+++ src/coponents/Hook.jsx
@@ -0,0 +1,5 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+const useFruitSelector = useSelector;
+const useFruitDispatch = useDispatch;
+export { useFruitDispatch, useFruitSelector };
